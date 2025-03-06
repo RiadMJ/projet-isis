a90691f1baad7d48213208ab892057b68d8559cd
Riad MALKI JAMAI <rmalkijamai@gmail.com>
2025-03-06 15:28:53 +0100

    First commit

---
+++ src/main/java/mainPackage/ProjetJeuxIsis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mainPackage;
+
+/**
+ *
+ * @author hp
+ */
+public class ProjetJeuxIsis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
